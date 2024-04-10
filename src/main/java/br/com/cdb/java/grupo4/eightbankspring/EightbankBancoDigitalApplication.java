cc89fba9d779be2bd71fb54328dbddae48c7ce3d
Marcelo de Sousa Silva <sousams93@outlook.com>
2024-04-10 11:09:54 -0300

    Initial commit

---
+++ src/main/java/br/com/cdb/java/grupo4/eightbankspring/EightbankBancoDigitalApplication.java
@@ -0,0 +1,13 @@
+package br.com.cdb.java.grupo4.eightbankspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EightbankBancoDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EightbankBancoDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cdb/java/grupo4/eightbankspring/EightbankBancoDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cdb.java.grupo4.eightbankspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EightbankBancoDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
